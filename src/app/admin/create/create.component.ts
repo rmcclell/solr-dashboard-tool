791aa8019c0df5529f60edca5c3397ed68ba6e59
Ryan McClelland <rmcclelland@revgen.com>
2020-07-10 17:54:17 -0600

    create and edit wizard stubs

--- src/app/admin/create/create.component.ts
+++ src/app/admin/create/create.component.ts
@@ -1,6 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
@@ -8,8 +28,16 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class CreateComponent implements OnInit {
   firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup
+  secondFormGroup: FormGroup;
+  thirdFormGroup: FormGroup;
+  forthFormGroup: FormGroup;
   isEditable: boolean = true;
+
+  displayedColumns = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
+  dataSource2 = ELEMENT_DATA;
+  dataSource3 = ELEMENT_DATA;
+  dataSource4 = ELEMENT_DATA;
 
   constructor(private _formBuilder: FormBuilder) {}
 
@@ -17,9 +45,15 @@ export class CreateComponent implements OnInit {
     this.firstFormGroup = this._formBuilder.group({
       firstCtrl: ['', Validators.required]
     });
-    this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required]
-    });
+    //this.secondFormGroup = this._formBuilder.group({
+    //secondCtrl: ['', Validators.required]
+    //});
+    //this.thirdFormGroup = this._formBuilder.group({
+    // thirdCtrl: ['', Validators.required]
+    //});
+    //this.forthFormGroup = this._formBuilder.group({
+    // forthCtrl: ['', Validators.required]
+    //});
   }
 
 }
