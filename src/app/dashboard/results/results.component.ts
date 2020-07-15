c864bacb3d58a0cf7bb64c9a9a02ce667b988b7e
Ryan McClelland <rmcclelland@revgen.com>
2020-07-15 11:19:45 -0600

    export csv

--- src/app/dashboard/results/results.component.ts
+++ src/app/dashboard/results/results.component.ts
@@ -3,6 +3,7 @@ import { Component, AfterViewInit, OnInit, ViewChild } from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 
+import { saveAs } from 'file-saver';
 import {merge, Observable, of as observableOf} from 'rxjs';
 import {catchError, map, startWith, switchMap} from 'rxjs/operators';
 
@@ -31,10 +32,10 @@ export class ResultsComponent implements OnInit, AfterViewInit  {
   constructor(private _httpClient: HttpClient) { }
 
   ngOnInit(): void {
+    this.solrData = new SolrHttpIndex(this._httpClient);
   }
 
   ngAfterViewInit() {
-    this.solrData = new SolrHttpIndex(this._httpClient);
 
     // If the user changes the sort order, reset back to the first page.
     this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
@@ -63,10 +64,24 @@ export class ResultsComponent implements OnInit, AfterViewInit  {
   formatColumnHeader(header: string) {
     return header.split(/(?=[A-Z])/).join(' ');
   }
+  exportResults() {
+    this.solrData.getExport(this.sort.active, this.sort.direction, this.displayedColumns.join(',')).subscribe(function(data){
+      var blob = new Blob([data], {type: "text/csv;charset=utf-8"});
+      saveAs(blob, "export.csv");
+    });
+  }
 }
 
 export class SolrHttpIndex {
   constructor(private _httpClient: HttpClient) {}
+
+  getExport(sort: string, order: string, fieldsList: string): Observable<any> {
+    const href = 'http://localhost:8983/solr/cats/select';
+    const requestUrl =
+        `${href}?q=country%3A(%5B%22United%20States%22%20TO%20%22United%20States%22%5D)&wt=csv&indent=true&fl=${fieldsList}&sort=${sort}%20${order}&rows=1000`;
+        http://localhost:8983/solr/cats/select
+    return this._httpClient.get(requestUrl, { responseType: 'text' });
+  }
 
   getData(sort: string, order: string, page: number, pageSize: number, fieldsList: string): Observable<Result> {
     const href = 'http://localhost:8983/solr/cats/select';
