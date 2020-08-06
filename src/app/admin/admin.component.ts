a667cad77df7f7b6f0551d86300493f58bd381e0
Ryan McClelland <rmcclelland@revgen.com>
2020-08-06 14:12:21 -0600

    Update admin grid

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -3,34 +3,19 @@ import { Component, OnInit } from '@angular/core';
 import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 import {MatTableDataSource} from '@angular/material/table';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
+export interface DashboardConfig {
+  description: string;
+  title: string;
+  active: boolean;
+  default: boolean;
   weight: number;
   symbol: string;
+  created: Date;
+  modified: Date;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 12, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 13, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 14, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 15, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 16, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 17, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 18, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 19, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 20, name: 'Neon', weight: 20.1797, symbol: 'Ne'}
+const DATA: DashboardConfig[] = [
+  {active: true, default: true, title: 'Cats', description: 'Hydrogen', weight: 1.0079, symbol: 'H', created: new Date(), modified: new Date() }
 ];
 
 @Component({
@@ -40,8 +25,8 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class AdminComponent implements OnInit {
 
-  columns: string[] = ['actions', 'position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  columns: string[] = ['actions', 'active', 'default', 'title', 'description', 'weight', 'symbol', 'created', 'modified'];
+  dataSource = new MatTableDataSource(DATA);
 
   drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.columns, event.previousIndex, event.currentIndex);
